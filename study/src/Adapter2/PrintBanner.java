df0069597c40c6499ba843d28f987cb5adedb03f
YuYoHan <zxzz8014@naver.com>
2000-07-16 10:45:00 +0000

    [Study] Adapter

---
+++ study/src/Adapter2/PrintBanner.java
@@ -0,0 +1,21 @@
+package Adapter2;
+
+import Adapter.Banner;
+
+public class PrintBanner extends Print{
+    private Banner banner;
+
+    public PrintBanner(Banner banner) {
+        this.banner = banner;
+    }
+
+    @Override
+    public void printWeak() {
+        banner.showWithParen();
+    }
+
+    @Override
+    public void printStrong() {
+        banner.showWithAster();
+    }
+}
