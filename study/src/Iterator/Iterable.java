870ec739cb0ece6aec2a81b3fbcc7209be035513
YuYoHan <zxzz8014@naver.com>
2023-01-17 13:20:51 +0900

    [Study] DesginPattern_Iterator

---
+++ study/src/Iterator/Iterable.java
@@ -0,0 +1,8 @@
+package Iterator;
+
+import java.util.Iterator;
+
+public interface Iterable<E> {
+    // 이 메소드는 집합체에 대응하는 Iterator<E>를 만들기 위한 것입니다.
+    public abstract Iterator<E> iterator();
+}
---
+++ study/src/Iterator/Iterator.java
@@ -0,0 +1,7 @@
+package Iterator;
+
+public interface Iterator<E> {
+    public abstract boolean hasNext();
+    public abstract E next();
+
+}
