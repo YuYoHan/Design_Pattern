29efaf0803ca933475d66aecf8229f0898457bab
YuYoHan <zxzz8014@naver.com>
2023-01-19 12:49:47 +0900

    [Study] Iterator

--- study/src/Aggregator.java
+++
@@ -1,5 +0,0 @@
-import java.util.Iterator;
-
-public interface Aggregator {
-    Iterator iterator();
-}
--- study/src/Iterator.java
+++
@@ -1,8 +0,0 @@
-public interface Iterator {
-    // Iterator을 통해서 Aggregator의 다음 구성 데이터를 얻을 수 있게하고
-    // 얻을 수 있으면 true, 없으면 false
-    boolean next();
-    // 구성 데이터를 하나 얻어 반환하는데
-    // Object 타입으로 반환한다.
-    Object current();
-}
