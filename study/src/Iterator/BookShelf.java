8882011c0c5c71d60d78ffe5ae79a52e559ad19b
YuYoHan <zxzz8014@naver.com>
2023-01-19 13:07:16 +0900

    [Study] array →  list

--- study/src/Iterator/BookShelf.java
+++ study/src/Iterator/BookShelf.java
@@ -1,25 +1,28 @@
 package Iterator;
+
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class BookShelf implements Iterable<Book>{
-    private Book[] books;
+    private List<Book> books;
     private int last =0;
 
-    public BookShelf(int maxsize) {
-        this.books = new Book[maxsize];
+    public BookShelf(int last) {
+        this.books = new ArrayList<Book>(last);
     }
 
     public Book getBookAt(int index) {
-        return books[index];
+        return books.get(index);
     }
 
     public void appendBook(Book book) {
-        this.books[last] = book;
+        this.books.add(last, book);
         last++;
     }
 
     public int getLength() {
-        return last;
+        return books.size();
     }
 
     @Override
--- study/src/Iterator/Main.java
+++ study/src/Iterator/Main.java
@@ -9,6 +9,9 @@ public class Main {
         bookShelf.appendBook(new Book("Bible"));
         bookShelf.appendBook(new Book("Cinderella"));
         bookShelf.appendBook(new Book("JAVA"));
+        bookShelf.appendBook(new Book("Spring"));
+        bookShelf.appendBook(new Book("SpringBoot"));
+        bookShelf.appendBook(new Book("JPA"));
 
         // 명시적으로 Iterator를 사용하는 방법
         Iterator<Book> it = bookShelf.iterator();
@@ -18,10 +21,5 @@ public class Main {
         }
         System.out.println();
 
-//        // 확장 for문을 사용하는 방법
-//        for(Book book : bookShelf) {
-//            System.out.println(book.getName());
-//        }
-//        System.out.println();
     }
 }
