2c94851dfde1f56eb1264dba2435a7c24754c984
YuYoHan <zxzz8014@naver.com>
2023-01-21 09:47:07 +0900

    [Study] Singleton

---
+++ study/src/Singleton/HelloWorld.java
@@ -0,0 +1,24 @@
+package Singleton;
+
+class Singleton {
+    private static class singleInstanceHolder {
+        private static final Singleton INSTANCE = new Singleton();
+    }
+    public static Singleton getInstance() {
+        return singleInstanceHolder.INSTANCE;
+    }
+}
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Singleton a = Singleton.getInstance();
+        Singleton b = Singleton.getInstance();
+        System.out.println(a.hashCode());
+        System.out.println(b.hashCode());
+
+        if (a == b) {
+            System.out.println(true);
+        }
+    }
+}
