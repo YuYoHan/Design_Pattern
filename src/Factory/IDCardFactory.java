7801ed47d08cdc52c46038f2cc5549c35694bef4
YuYoHan <zxzz8014@naver.com>
2023-07-05 21:14:21 +0900

    [Study] factory method 패턴

---
+++ src/Factory/IDCardFactory.java
@@ -0,0 +1,14 @@
+package Factory;
+
+public class IDCardFactory extends Factory{
+
+    @Override
+    protected Product createProduct(String owner) {
+        return new IDCard(owner);
+    }
+
+    @Override
+    protected void registerProduct(Product product) {
+        System.out.println(product + "을 등록했습니다.");
+    }
+}
---
+++ src/Factory/Product.java
@@ -0,0 +1,5 @@
+package Factory;
+
+public abstract class Product {
+    public abstract void use();
+}
