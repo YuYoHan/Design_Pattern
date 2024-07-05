3d5bebe47837d1c8f06422a89af28435975fbf92
유요한 <110465572+YuYoHan@users.noreply.github.com>
2024-07-05 11:49:10 +0900

    [Study] abstract_factory 패턴

---
+++ study/src/abstract_factory/ListPage.java
@@ -0,0 +1,25 @@
+package abstract_factory;
+
+public class ListPage extends Page {
+
+    public ListPage(String title, String author) {
+        super(title, author);
+    }
+
+    @Override
+    public String makeHTML() {
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("<html><head><title>" + title + "</title></head>\n");
+        buffer.append("<body>\n");
+        buffer.append("<h1>" + title + "</h1>\n");
+        buffer.append("<ul>\n");
+        for (Object o: content) {
+            Item item = (Item) o;
+            buffer.append(item.makeHTML());
+        }
+        buffer.append("</ul>\n");
+        buffer.append("<hr><address>" + author + "</address>");
+        buffer.append("</body></html>");
+        return buffer.toString();
+    }
+}
